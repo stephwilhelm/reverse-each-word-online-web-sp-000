ccd504486287ac3041f426184b7912aa5da9b7f7
Steph W <wilhelm.steph@gmail.com>
2020-04-07 22:05:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence1 = "Hello there, and how are you?"
 
 def reverse_each_word(sentence1)
-	sentence1.split(" ") do |sentence1.reverse|
+	sentence1.split(" ").reverse do
     puts "#{sentence1.reverse}"
   end
 end
